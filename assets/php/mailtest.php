033c2ad37171f7ebdafad0a882004d2b83fc572e
hajnina <hajnina11@gmail.com>
2017-01-07 22:18:53 +0100

    Test mailove funkce

---
+++ assets/php/mailtest.php
@@ -0,0 +1,21 @@
+<html>
+    <?php
+        include "assets/php/databaze.php";
+        $random=rand();
+    ?>
+    <body>
+        <form method="post">
+            <input name="Komu" /> E-mail příjemce<br />
+            <input name="Predmet" /> Předmět<br />
+            Obsah zprávy:<br />
+            <textarea name="Text"></textarea><br />
+            <input type="submit" value="odeslat" name="cudlik" />
+        </form>
+       <?php
+            if(isset($_POST["Komu"])){
+                $ok=poslimail($_POST["Komu"],$_POST["Predmet"],$_POST["Text"]);
+                if($ok){echo "OK - Mail odeslán";}else{echo "ERROR - Mail neodeslán, někde se stala chyba";}
+            }
+        ?>
+    </body>
+</html>
