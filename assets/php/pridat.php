e07fdcdc0ced01f0d3c00f31e4dda19f846a1106
hajnina <hajnina11@gmail.com>
2017-02-25 14:36:10 +0100

    hotfix
    FUNGUJE TO!!!! lidská blbost je nekonečná.....

--- assets/php/pridat.php
+++ assets/php/pridat.php
@@ -1,4 +1,6 @@
 <?php
+    include "databaze.php";
+
     loguj("Pokus o přidání nového uživatele...");
     if(isset($_POST["Jmeno"]) && isset($_POST["Vek"]) && isset($_POST["Email"]) && isset($_POST["Bydliste"]) && isset($_POST["Cinnost"]) && isset($_POST["Destinace"])){
         
@@ -14,5 +16,8 @@
         
         $ok=pridej($Jmeno, $Vek, $Email, $Bydliste, $Cinnost, $Destinace);
         
+    }
+    else{
+        loguj("issetová zkouška neuspěla, asi nebyly vyplněny všechny parametry");
     }
 ?>
