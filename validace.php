af8666196408bb8d3b42e63285e9126a50d3a2af
hajnina <hajnina11@gmail.com>
2017-02-26 20:49:20 +0100

    geo2name fix

--- validace.php
+++ validace.php
@@ -47,12 +47,12 @@
                 //v tomhle bodě se našel vhodný match a měl by se obou odeslat mail alá "nazdar, našli jsme vám strašně super parťáka" apod...
                 $partak = mysqli_fetch_array(dotaz("SELECT Id, Jmeno, Destinace, Bydliste, Email, Vek FROM lidi WHERE Id=".$partak[4].";"));
                 
-                posliMail($ja[4],"Dámetrip.cz - Našli jsme ti parťáka!","Ahoj ".$ja[1].",\nNašli jsme ti parťáka. Jmenuje se ".$partak[1].", je z ".geo2name($partak[3]).", je mu ".$partak[5]." a chce jet do ".$partak[2].". \nNapiš mu na ".$partak[4]." a vyražte spolu na super trip!");
+                posliMail($ja[4],"Dámetrip.cz - Našli jsme ti parťáka!","Ahoj ".$ja[1].",\nNašli jsme ti parťáka. Jmenuje se ".$partak[1].", je z ".geo2name($partak[3]).", je mu ".$partak[5]." a chce jet do ".geo2name($partak[2]).". \nNapiš mu na ".$partak[4]." a vyražte spolu na super trip!");
                 
-                posliMail($partak[4],"Dámetrip.cz - Našli jsme ti parťáka!","Ahoj ".$partak[1].",\nNašli jsme ti parťáka. Jmenuje se ".$ja[1].", je z ".geo2name($ja[3]).", je mu ".$ja[5]." a chce jet do ".$ja[2].". \nNapiš mu na ".$ja[4]." a vyražte spolu na super trip!");
+                posliMail($partak[4],"Dámetrip.cz - Našli jsme ti parťáka!","Ahoj ".$partak[1].",\nNašli jsme ti parťáka. Jmenuje se ".$ja[1].", je z ".geo2name($ja[3]).", je mu ".$ja[5]." a chce jet do ".geo2name($ja[2]).". \nNapiš mu na ".$ja[4]." a vyražte spolu na super trip!");
                 
-                dotaz("UPDATE lidi SET Aktivni=0 WHERE Id=".$ja[0]";");
-                dotaz("UPDATE lidi SET Aktivni=0 WHERE Id=".$partak[0]";");
+                dotaz("UPDATE lidi SET Aktivni=0 WHERE Id=".$ja[0].";");
+                dotaz("UPDATE lidi SET Aktivni=0 WHERE Id=".$partak[0].";");
             } 
             else {
                 //vhodného parťáka se najít nepodařilo... v podstatě teď tomuto uživateli nezbývá nic jiného, než čekat, až někdo pojede poblíž a vybere si ho jako parťáka
